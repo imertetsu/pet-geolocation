227d9793fff1550bdf4ca71efa1821d450282750
imertetsu <imertpro@gmail.com>
2025-07-24 22:00:36 -0400

    adding user case for newsPost

--- pets-location/src/main/java/com/pets/application/news/GetNewsPostByIdUseCase.java
+++ pets-location/src/main/java/com/pets/application/news/GetNewsPostByIdUseCase.java
@@ -1,2 +1,20 @@
-package com.pets.application.news;public class GetNewsPostByIdUseCase {
+package com.pets.application.news;
+
+import com.pets.domain.model.NewsPost;
+import com.pets.domain.repository.NewsRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GetNewsPostByIdUseCase {
+
+    private final NewsRepository newsRepository;
+
+    public GetNewsPostByIdUseCase(NewsRepository newsRepository) {
+        this.newsRepository = newsRepository;
+    }
+
+    public NewsPost execute(Long postId) {
+        return newsRepository.findById(postId)
+                .orElseThrow(() -> new RuntimeException("News post not found with ID: " + postId));
+    }
 }
