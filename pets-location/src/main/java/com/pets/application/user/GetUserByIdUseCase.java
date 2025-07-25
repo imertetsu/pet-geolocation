5531b7a9ddeea86f25db322c67be12eeca412f23
imertetsu <imertpro@gmail.com>
2025-07-24 22:02:22 -0400

    adding use case to get user by id

--- pets-location/src/main/java/com/pets/application/user/GetUserByIdUseCase.java
+++ pets-location/src/main/java/com/pets/application/user/GetUserByIdUseCase.java
@@ -1,2 +1,22 @@
-package com.pets.application.user;public class GetUserByIdUseCase {
+package com.pets.application.user;
+
+import com.pets.domain.model.User;
+import com.pets.domain.repository.UserRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class GetUserByIdUseCase {
+
+    private final UserRepository userRepository;
+
+    public GetUserByIdUseCase(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User execute(UUID userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
 }
